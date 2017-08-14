2aac975adc092a2e2963555c7bfa956e734e5c02
Aliaksei_Rachanok <bagarottiz@gmail.com>
2017-08-14 11:31:03 +0300

    upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,56 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const propsValues = {
+  items: [
+            "Hello",
+            "World"
+  ]
+};
+
+class Item extends Component {
+    render() {
+        return (
+            <li>{this.props.name}</li>
+        );
+    }
+}
+
+class Button extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {class: "off", label: "Press"};
+
+        this.press = this.press.bind(this);
+    }
+
+    press() {
+        var className = (this.state.class==='off')?'on':'off';
+        this.setState({class: className});
+    }
+
+    render() {
+        return <button onClick={this.press} className={this.state.class}>{this.state.label}</button>
+    }
+}
+
+class Hello extends Component {
+  render() {
+    return (
+        <div>
+          <ul>
+              {
+                  this.props.data.items.map(function(item){
+                  return <Item key={item} name={item} />
+                  })
+              }
+          </ul>
+        </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +63,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Hello data={propsValues}/>
+        <Button />
       </div>
     );
   }
