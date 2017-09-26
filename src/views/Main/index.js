37d11778ba69fb4019956ae38d9e09ce5ff645df
Yuliya_Khodzikava <ykhodzikava@copyright.com>
2017-09-26 10:32:02 +0300

    updates

--- src/views/Main/index.js
+++ src/views/Main/index.js
@@ -6,8 +6,8 @@ class Main extends Component {
     render() {
         return (
            <main>
-               <div className="blog-items">
-                   <div className="item-1">
+               <div className="BlogItem">
+                   <div className="blog-item">
                        <h1><a href="">Lorem ipsum dolor sit amet</a></h1>
                        <a href=""><img src="img/mountains-clouds-forest-fog.jpg" alt="pic1"/></a>
                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi iusto optio, sed totam debitis perferendis in voluptatibus exercitationem dolores aspernatur aliquid, porro fuga molestias consequuntur atque culpa repudiandae fugiat sit! Voluptatibus numquam tenetur voluptate ut vel voluptas libero nulla quisquam. Illo fugiat autem esse voluptatum voluptas atque, commodi minus natus!Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
@@ -20,7 +20,7 @@ class Main extends Component {
                            <li><a href=""><mark>iste id</mark></a></li>
                        </ul>
                    </div>
-                   <div className="item-2">
+                   <div className="blog-item">
                        <h1><a href="">Lorem ipsum dolor sit amet</a></h1>
                        <a href=""><img src="img/black-and-white-animal-dog-pet.jpg" alt="pic2"/></a>
                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi iusto optio, sed totam debitis perferendis in voluptatibus exercitationem dolores aspernatur aliquid, porro fuga molestias consequuntur atque culpa repudiandae fugiat sit! Voluptatibus numquam tenetur voluptate ut vel voluptas libero nulla quisquam. Illo fugiat autem esse voluptatum voluptas atque, commodi minus natus!Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
@@ -33,7 +33,7 @@ class Main extends Component {
                            <li><a href=""><mark>iste id</mark></a></li>
                        </ul>
                    </div>
-                   <div className="item-3">
+                   <div className="blog-item">
                        <h1><a href="">Lorem ipsum dolor sit amet</a></h1>
                        <a href=""><img src="img/pexels-photo-331991.jpeg" alt="pic3"/></a>
                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi iusto optio, sed totam debitis perferendis in voluptatibus exercitationem dolores aspernatur aliquid, porro fuga molestias consequuntur atque culpa repudiandae fugiat sit! Voluptatibus numquam tenetur voluptate ut vel voluptas libero nulla quisquam. Illo fugiat autem esse voluptatum voluptas atque, commodi minus natus!Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
