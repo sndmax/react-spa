f22ac03636a32c785537a57393c1cd51e4fbafeb
Yuliya_Khodzikava <ykhodzikava@copyright.com>
2017-10-25 18:29:17 +0300

    userpage_update

--- src/views/Userpage/index.js
+++ src/views/Userpage/index.js
@@ -16,36 +16,39 @@ class Userpage extends Component {
                              };
 
         return (
-            <div className="userpage">
-                <div className="profile">
-                	<h2 className="profile__header">Blogger&#39;s info&#58;</h2>
-                	<div className="profile__avatar"></div>
-                	<ul className="profile__info">
-                		<li>First Name:
-                			<p className="profile__info--bold">Julia</p>
+            <div className="c-userpage">
+                <div className="c-userpage__info">
+                    <h2 className="c-userpage__header c-userpage__header--padding">Blogger&#39;s info&#58;</h2>
+                    <div className="c-userpage__avatar"></div>
+                    <ul className="c-userpage__card">
+                        <li>
+                            <span className="c-userpage__field">First Name:</span>
+                            <p className="c-userpage__value c-userpage__value--bold">Julia</p>
+                        </li>
+                		<li>
+                            <span className="c-userpage__field">Last Name:</span>
+                			<p className="c-userpage__value c-userpage__value--bold">Hodikova</p>
                 		</li>
-                		<li>Last Name:
-                			<p className="profile__info--bold">Hodikova</p>
+                		<li>
+                		    <span className="c-userpage__field">E-mail address:</span>
+                			<p className="c-userpage__value c-userpage__value--bold">jhodikova@mail.ru</p>
                 		</li>
-                		<li>E-mail address:
-                			<p className="profile__info--bold">jhodikova@mail.ru</p>
-                		</li>
-                	</ul>
+                    </ul>
                 </div>
 
-                <div className="profile__about">
-                	<h3 className="profile__about__header">About&#58;</h3>
-                	<p className="profile__about__text profile__about--font_size">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia architecto assumenda, quisquam delectus officia perspiciatis eius nobis fugit optio iure! Soluta, necessitatibus quod, ea rerum illo quo laboriosam nobis excepturi?
+                <div className="c-userpage__about">
+                	<h3 className="c-userpage__header">About&#58;</h3>
+                	<p className="c-userpage__text c-userpage__text--font_size">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia architecto assumenda, quisquam delectus officia perspiciatis eius nobis fugit optio iure! Soluta, necessitatibus quod, ea rerum illo quo laboriosam nobis excepturi?
                 	</p>
                 </div>
 
-                <div className="profile__posts">
-                	<h3 className="profile__posts__header">User&#39;s posts&#58;</h3>
-                	<ul className="profile__posts__list">
-                		<li><a href="#">Lorem ipsum | <span className="profile__posts__list__date">05.08.2016</span></a></li>
-                		<li><a href="#">Lorem ipsum dolor | <span className="profile__posts__list__date">01.02.2017</span></a></li>
-                		<li><a href="#">Lorem | <span className="profile__posts__list__date">12.08.2015</span></a></li>
-                		<li><a href="#">Lorem ips | <span className="profile__posts__list__date">07.07.2007</span></a></li>
+                <div className="c-userpage__posts">
+                	<h3 className="c-userpage__header">User&#39;s posts&#58;</h3>
+                	<ul className="l-list">
+                		<li><a href="#">Lorem ipsum | <span className="l-list__item">05.08.2016</span></a></li>
+                		<li><a href="#">Lorem ipsum dolor | <span className="l-list__item">01.02.2017</span></a></li>
+                		<li><a href="#">Lorem | <span className="l-list__item">12.08.2015</span></a></li>
+                		<li><a href="#">Lorem ips | <span className="l-list__item">07.07.2007</span></a></li>
                 	</ul>
                 </div>
             </div>
